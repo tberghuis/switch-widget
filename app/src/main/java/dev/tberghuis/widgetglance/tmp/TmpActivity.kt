0b338fa324909d18dc86d86cbd55129f35fc2b34
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-10-18 10:04:30 +1100

    .

--- app/src/main/java/dev/tberghuis/widgetglance/tmp/TmpActivity.kt
+++ app/src/main/java/dev/tberghuis/widgetglance/tmp/TmpActivity.kt
@@ -1,5 +1,6 @@
 package dev.tberghuis.widgetglance.tmp
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,12 +8,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dev.tberghuis.widgetglance.logd
 import dev.tberghuis.widgetglance.ui.theme.WidgetGlanceTheme
 
 
 class TmpActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+
+    val action: String? = intent?.action
+    val data: Uri? = intent?.data
+    logd("TmpActivity onCreate action: $action")
+    logd("TmpActivity onCreate data: $data")
 
     setContent {
       WidgetGlanceTheme {
