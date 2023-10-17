c4a15ba21aeafdc913968a07be026b9c7c78bbae
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-10-18 09:12:54 +1100

    .

---
+++ app/src/main/java/dev/tberghuis/widgetglance/tmp/TmpScreen.kt
@@ -0,0 +1,12 @@
+package dev.tberghuis.widgetglance.tmp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TmpScreen() {
+  Column {
+    Text("tmp screen")
+  }
+}
