856f65cb6e4472444b882447f5461c22190835b2
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-10-20 13:28:15 +1100

    .

--- app/src/main/java/dev/tberghuis/widgetglance/deeplink/DeepLinkActivity.kt
+++ app/src/main/java/dev/tberghuis/widgetglance/deeplink/DeepLinkActivity.kt
@@ -1,7 +1,5 @@
 package dev.tberghuis.widgetglance.deeplink
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,13 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import dev.tberghuis.widgetglance.logd
 import dev.tberghuis.widgetglance.ui.theme.WidgetGlanceTheme
 
 class DeepLinkActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    handleIntent(intent)
     setContent {
       WidgetGlanceTheme {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
@@ -23,17 +19,5 @@ class DeepLinkActivity : ComponentActivity() {
         }
       }
     }
-  }
-
-  override fun onNewIntent(intent: Intent) {
-    super.onNewIntent(intent)
-    handleIntent(intent)
-  }
-
-  private fun handleIntent(intent: Intent) {
-    val action: String? = intent.action
-    val data: Uri? = intent.data
-    logd("TmpActivity onCreate action: $action")
-    logd("TmpActivity onCreate data: $data")
   }
 }
