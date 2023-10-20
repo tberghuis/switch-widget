732539ede2c9e21e7049668816b51444e31c897e
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-10-20 13:26:13 +1100

    .

--- app/src/main/java/dev/tberghuis/widgetglance/MainActivity.kt
+++ app/src/main/java/dev/tberghuis/widgetglance/MainActivity.kt
@@ -1,6 +1,5 @@
 package dev.tberghuis.widgetglance
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,16 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.core.content.pm.ShortcutManagerCompat
 import dev.tberghuis.widgetglance.screens.HomeScreen
 import dev.tberghuis.widgetglance.ui.theme.WidgetGlanceTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-//    tmp(this)
-
     setContent {
       WidgetGlanceTheme {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
@@ -27,9 +22,3 @@ class MainActivity : ComponentActivity() {
     }
   }
 }
-
-
-//fun tmp(context: Context){
-//  val count = ShortcutManagerCompat.getMaxShortcutCountPerActivity(context)
-//  logd("MainActivity getMaxShortcutCountPerActivity $count")
-//}
